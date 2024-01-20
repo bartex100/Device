d302bc93269df35824e45e9de76dc7be94def7a5
Bartek Koszutowski <koszutowski.bartek@gmail.com>
2024-01-20 12:01:29 +0100

    push

---
+++ Device/Device.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Device
+{
+    internal class Device
+    {
+        int height, width;
+        string company;
+        public int Height { get { return height; } set { height = value; } }
+        public int Width { get { return width; } set { width = value; } }
+        public string Company { get { return company; } set { company = value; } }
+    }
+}
